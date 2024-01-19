75893c55623395c7110b6db285a6bd4611bfa79b
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-01-19 17:19:52 +0530

    update

---
+++ frontend/script/login.js
@@ -0,0 +1,89 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container-1");
+
+// Animations
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+const register_btn = document.querySelector("#register");
+const login_btn = document.querySelector("#log-in");
+
+register_btn.addEventListener("click", registerUser);
+login_btn.addEventListener("click", loginUser);
+
+// BaseURL
+const baseURL = "";
+
+// handling registration
+async function registerUser() {
+  //getting elements
+  const username_input = document.getElementById("sign-in-username");
+  const password_input = document.getElementById("sign-in-password");
+  const email_input = document.getElementById("sign-in-email");
+  //getting values
+  const username = username_input.value;
+  const password = password_input.value;
+  const email = email_input.value;
+  if (username && password && email) {
+    const res = await fetch(`${baseURL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password, email }),
+    });
+    // const data = await res.json();
+    if (res.status == 200) {
+      container.classList.remove("sign-up-mode");
+    } else {
+      checkCredentials("Invalid Credentials");
+    }
+  } else {
+    checkCredentials("Empty Fields!!");
+  }
+}
+
+// Handling login
+async function loginUser() {
+  // getting elements
+  const username_input = document.getElementById("log-in-username");
+  const password_input = document.getElementById("log-in-password");
+  // getting values
+  const username = username_input.value;
+  const password = password_input.value;
+  if (username && password) {
+    const res = await fetch(`${baseURL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    if (res.status == 200) {
+      const data = await res.json();
+      localStorage.setItem("token", data.token);
+      // location.href
+    } else {
+      checkCredentials("Invalid Credentials");
+    }
+  } else {
+    checkCredentials("Empty Fields!!");
+  }
+}
+
+/// function to handle Invalid credentials
+async function checkCredentials(message) {
+  const invalidspan = document.getElementById("invalid-text");
+  const invaliddiv = document.getElementById("invalid");
+  invalidspan.innerHTML = message;
+  invaliddiv.classList.remove("invalid-invisible");
+  setTimeout(() => {
+    invaliddiv.classList.add("invalid-invisible");
+  }, 1500);
+}
