484d06c8e47744b6b25b74257e209f82613b2471
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-01-19 16:07:47 +0530

    update

---
+++ frontend/script/userdashboard.js
@@ -0,0 +1,166 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 20.5937, lng: 78.9629 },
+    zoom: 13,
+  });
+
+  const startInput = document.getElementById("startLocation");
+  const destinationInput = document.getElementById("destinationLocation");
+
+  const startSearchBox = new google.maps.places.SearchBox(startInput);
+  const destinationSearchBox = new google.maps.places.SearchBox(
+    destinationInput
+  );
+
+  map.addListener("bounds_changed", function () {
+    startSearchBox.setBounds(map.getBounds());
+    destinationSearchBox.setBounds(map.getBounds());
+  });
+
+  let startMarker;
+  let destinationMarker;
+
+  startSearchBox.addListener("places_changed", function () {
+    const places = startSearchBox.getPlaces();
+    if (places.length === 0) return;
+
+    if (startMarker) {
+      startMarker.setMap(null);
+      startMarker = null;
+    }
+
+    startMarker = new google.maps.Marker({
+      map: map,
+      title: places[0].name,
+      position: places[0].geometry.location,
+      draggable: false,
+      icon: "../icons/riding.png",
+      animation: google.maps.Animation.DROP,
+    });
+
+    calculateAndDisplayRoute();
+  });
+
+  destinationSearchBox.addListener("places_changed", function () {
+    const places = destinationSearchBox.getPlaces();
+    if (places.length === 0) return;
+
+    if (destinationMarker) {
+      destinationMarker.setMap(null);
+    }
+
+    destinationMarker = new google.maps.Marker({
+      map: map,
+      title: places[0].name,
+      position: places[0].geometry.location,
+      draggable: false,
+      icon: "../icons/cabstand.png",
+      animation: google.maps.Animation.DROP,
+    });
+
+    calculateAndDisplayRoute();
+  });
+
+  // Rendering Direction
+  let directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      strokeColor: "#000000",
+    },
+  });
+  directionsRenderer.setMap(map);
+
+  /// displaying route
+  function calculateAndDisplayRoute() {
+    if (startMarker && destinationMarker) {
+      const request = {
+        origin: startMarker.getPosition(),
+        destination: destinationMarker.getPosition(),
+        travelMode: "DRIVING",
+      };
+
+      directionsService.route(request, function (result, status) {
+        if (status === "OK") {
+          directionsRenderer.setDirections(result);
+          const distance = result.routes[0].legs[0].distance.text;
+          const source = startMarker.getPosition().toJSON();
+          const destination = destinationMarker.getPosition().toJSON();
+          showCarsPanel(source, destination, distance);
+        } else {
+          console.error("Directions request failed with status:", status);
+        }
+      });
+    }
+  }
+}
+
+// initMap();
+
+async function showCarsPanel(source, destination, distance) {
+  const confirmbtn = document.getElementById("confirm");
+  confirmbtn.classList.add("confirm-fill");
+  confirmbtn.removeEventListener("click", myfunction);
+  confirmbtn.addEventListener("click", myfunction);
+  function myfunction() {
+    const carspanel = document.getElementById("cars-panel");
+    carspanel.innerHTML = "";
+    distance = Number.parseInt(distance);
+    carspanel.classList.toggle("cars-panel-invisible");
+    addcars(source, destination, distance);
+    var btnvalue = confirmbtn.value;
+    if (btnvalue == "Confirm") confirmbtn.value = "Cancel";
+    else if (btnvalue == "Cancel") confirmbtn.value = "Confirm";
+  }
+}
+
+// showCarsPanel();
+const baseurl = "http://localhost:8080";
+async function addcars(source, destination, distance) {
+  try {
+    const carspanel = document.getElementById("cars-panel");
+    carspanel.innerHTML = "";
+    const res = await fetch(`${baseurl}/carData`);
+    const { cars_data: cars } = await res.json();
+    cars.forEach((item, i, arr) => {
+      const { image, name, price } = item;
+      const totalfare = distance * 25 + Number(price);
+      const paneldiv = document.createElement("div");
+      paneldiv.classList.add("cars-panel-item");
+      paneldiv.classList.add(`panel-item-${i + 1}`);
+      paneldiv.addEventListener("click", () => {
+        const bookbutton = document.getElementById("book-ride");
+        bookbutton.innerHTML = `Request ${name}`;
+        removemarkerfrompanelitems();
+        paneldiv.classList.add("cars-panel-border-color");
+        sendnotificationToDriver(source, destination);
+      });
+      const carimage = document.createElement("img");
+      carimage.src = image;
+      const cartype = document.createElement("span");
+      cartype.innerHTML = name;
+      const fare = document.createElement("span");
+      fare.innerHTML = `₹${totalfare}`;
+      paneldiv.append(carimage, cartype, fare);
+      carspanel.append(paneldiv);
+    });
+    const bookbtn = document.createElement("button");
+    bookbtn.id = "book-ride";
+    bookbtn.innerHTML = `Choose Ride`;
+    carspanel.append(bookbtn);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// removing markers
+// removemarkerfrompanelitems();
+function removemarkerfrompanelitems() {
+  const panelItems = document.getElementsByClassName("cars-panel-item");
+  const panelItemsarray = Array.prototype.slice.call(panelItems);
+  panelItemsarray.forEach((item) => {
+    item.classList.remove("cars-panel-border-color");
+  });
+}
+
+//sendNotification to driver
+async function sendnotificationToDriver(source, destination) {}
