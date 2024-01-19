ab700e120a5b2e2b9e8233a5a83ec25deb1eac62
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-01-19 16:05:41 +0530

    index page update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const { connection } = require("./db");
 const { carDataRouter } = require("./routes/carData.route");
+const { userRouter } = require("./routes/user.route");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const app = express();
 
-const cors = require("cors");
-const { userRouter } = require("./routes/user.route");
-carDataRouter.use(cors())
+//middlewares
+app.use(cors());
+app.use(express.json());
 
-app.use("/carData", carDataRouter)
-app.use("users",userRouter)
+// swagger UI
+// my requirements
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "RiderX APIs",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+// building OpenApi Specifications
+const openApiSpec = swaggerJsDoc(options);
+
+//Building complete UI
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(openApiSpec));
+
+// routes
+app.use("/carData", carDataRouter);
+app.use("/users", userRouter);
+
 // connecting to server and DB
 app.listen(8080, async () => {
   try {
