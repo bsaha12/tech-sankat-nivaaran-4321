e167aaa4dd936c723ba10962550b5c6f7e99e0c4
DEEPAK101020 <deepakkumarktr1998@gmail.com>
2024-01-19 23:31:14 +0530

    swagger jagger

--- backend/middlewares/driver.auth.js
+++ backend/middlewares/driver.auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 
 const authLogout = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-
   if (token) {
     try {
       const decoded = jwt.verify(token, "sankat-nivaaran");
