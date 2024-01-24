5d4dafe49e2c047804d406966b5fe2a20dfda3cb
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-01-24 15:37:11 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use('frontend/uploads/', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://moonlit-trifle-686331.netlify.app');
- 
+ res.header('Access-Control-Allow-Origin','http://127.0.0.1:5501')
   next();
 });
 app.use(express.json());
